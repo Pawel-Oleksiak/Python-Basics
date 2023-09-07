c4acce98c5b15f053987f52f72613c8084b2c075
Pawel <paablitoo211@gmail.com>
2023-09-07 19:29:39 +0200

    added comments

--- simple_programs/acronym_creator.py
+++ simple_programs/acronym_creator.py
@@ -1,7 +1,9 @@
+# user enters phrase
 phrase = input("Enter a phrase: ")
 words = phrase.split()
 a = " "
 
+# program loops through each word separately
 for i in words:
     a = a + str(i[0].upper())
 print(a)
